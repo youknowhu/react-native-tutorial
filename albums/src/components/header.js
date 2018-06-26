e3773dac2da3f0c41cd6a5af516d5e8d42886bbd
youknowhu <kimberly.hu@gmail.com>
2018-06-25 21:47:35 -0700

    added header component

---
+++ albums/src/components/header.js
@@ -0,0 +1,12 @@
+// Import libraries for making component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make component
+
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+// Make component available to other parts of the app
+export default Header; 
