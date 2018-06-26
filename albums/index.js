8968f2734638e2d6cc7e3997410a29f25b0e5a99
youknowhu <kimberly.hu@gmail.com>
2018-06-25 17:31:27 -0700

    update indexjs

--- albums/index.js
+++ albums/index.js
@@ -1,14 +1,8 @@
-//Import a library to help create a component
+
 import React from 'react';
 import ReactNative from 'react-native';
-
 
 //Create a component
 
 
 //Render it to the device
-
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.registerComponent('albums', () => App);
