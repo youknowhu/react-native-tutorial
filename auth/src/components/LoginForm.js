f38dc1afaf783245c0991f1071f5fa775c9a6c4e
youknowhu <kimberly.hu@gmail.com>
2018-06-27 11:47:05 -0700

    create reusable input component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component { 
+  state = { text: '' };
+
+
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <Input
+            value={this.state.text} 
+            onChangeText={ text => this.setState({ text })}
+            />
+        </CardSection>
+        
         <CardSection />
         <CardSection>
           <Button>
